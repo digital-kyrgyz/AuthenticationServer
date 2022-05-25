bccbbf4dc6e8737fd64a507e19c175fa3b5eab81
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-05-25 18:05:14 +0600

    ResponseDto was created

---
+++ SharedLibrary/Dto/ResponseDto.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLibrary.Dto
+{
+    public class ResponseDto<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+        [JsonIgnore] public bool IsSuccesful { get; private set; }
+        public ErrorDto Error { get; private set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default, StatusCode = statusCode, IsSuccesful = true };
+        }
+
+        public static ResponseDto<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new ResponseDto<T> { Error = errorDto, StatusCode = statusCode, IsSuccesful = false };
+        }
+
+        public static ResponseDto<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new ResponseDto<T> { Error = errorDto, StatusCode = statusCode, IsSuccesful = false };
+        }
+    }
+}
