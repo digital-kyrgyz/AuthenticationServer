cf2c141c99c1fe21a63ea6b882975fadeb2dda05
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-05-26 09:45:30 +0600

    ITokenService created

---
+++ Core/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Configuration;
+using Core.Dtos;
+using Core.Entities;
+
+namespace Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp user);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
