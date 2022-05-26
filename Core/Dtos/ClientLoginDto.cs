5f13ce4202b21cc0e694dfe988f63ddb673cb09f
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-05-26 10:00:56 +0600

    IAuthenticationService created

---
+++ Core/Dtos/ClientLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Dtos
+{
+    public class ClientLoginDto
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ Core/Services/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Core.Dtos;
+using SharedLibrary.Dto;
+
+namespace Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<ResponseDto<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
