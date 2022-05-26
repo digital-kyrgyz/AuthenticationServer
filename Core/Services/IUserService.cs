e49352cb5f0a246942ee8c10ff155479e02ac80f
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-05-26 10:08:46 +0600

    IUserService created

---
+++ Core/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Core.Dtos;
+using SharedLibrary.Dto;
+
+namespace Core.Services
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<ResponseDto<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
