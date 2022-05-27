7dd79f3c41c2e824bb3a950e8428a2e5334957fe
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-05-27 16:39:42 +0600

    Implemented IUnitOfWork in the layer 'Repository'

---
+++ Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
